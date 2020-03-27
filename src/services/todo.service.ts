4be06c0bc8337a2c47847ce099f488eef45d3fc5
Lorenz Winkler <winlor01@gmail.com>
2020-03-27 17:22:42 +0100

    Add datetime value-converter

--- src/services/todo.service.ts
+++ src/services/todo.service.ts
@@ -5,71 +5,70 @@ import { inject } from 'aurelia-framework';
 
 @inject(ApiConfig)
 export class TodoService {
-    private httpClient: HttpClient;
-    constructor(private apiConf: ApiConfig) {
-        this.httpClient = new HttpClient();
+  private httpClient: HttpClient;
+  constructor(private apiConf: ApiConfig) {
+    this.httpClient = new HttpClient();
 
-    }
+  }
 
-    public async getTodos(): Promise<Todo[]> {
-        try {
-            let res = await this.httpClient.fetch(this.apiConf.basePath, {
-                method: 'get',
-            });
-            if (res.ok)
-                return <Todo[]>(await res.json());
-            else
-                return null
-        } catch (err) {
-            return null;
-        }
+  public async getTodos(): Promise<Todo[]> {
+    try {
+      let res = await this.httpClient.fetch(this.apiConf.basePath, {
+        method: 'get',
+      });
+      if (res.ok)
+        return <Todo[]>(await res.json());
+      else
+        return null
+    } catch (err) {
+      return null;
     }
-    public async getTodo(id: number) {
-        try {
-            let res = await this.httpClient.fetch(this.apiConf.basePath, {
-                method: 'get',
-            });
-            return (await res.ok) ? res.json() : null;
-        } catch (err) {
-            return null;
-        }
+  }
+  public async getTodo(id: number) {
+    try {
+      let res = await this.httpClient.fetch(this.apiConf.basePath+id, {
+        method: 'get',
+      });
+      return (await res.ok) ? res.json() : null;
+    } catch (err) {
+      return null;
     }
-    public async createTodo(todo: Todo): Promise<Todo> {
-        try {
-            console.log(json(todo));
-            console.log(JSON.stringify(todo));
-            let res = await this.httpClient.fetch(this.apiConf.basePath, {
-                method: 'post',
-                body: json(todo),
-            });
-            return (await res.ok) ? res.json() : null;
-        } catch (err) {
-            return null;
-        }
+  }
+  public async createTodo(todo: Todo): Promise<Todo> {
+    try {
+      let res = await this.httpClient.fetch(this.apiConf.basePath, {
+        method: 'post',
+        body: json(todo),
+      });
+      return (await res.ok) ? res.json() : null;
+    } catch (err) {
+      return null;
     }
-    public async updateTodo(todo: Todo): Promise<Todo> {
-        try {
-            let res = await this.httpClient.fetch(this.apiConf.basePath, {
-                method: 'put',
-                body: json(todo),
-            });
-            return (await res.ok) ? res.json() : null;
-        } catch (err) {
-            return null;
-        }
+  }
+  public async updateTodo(todo: Todo): Promise<Todo> {
+    try {
+      let res = await this.httpClient.fetch(this.apiConf.basePath, {
+        method: 'put',
+        body: json(todo),
+      });
+      return (await res.ok) ? res.json() : null;
+    } catch (err) {
+      console.log(err);
+      return null;
     }
-    public async deleteTodo(todo: Todo): Promise<boolean> {
-        return await this.deleteTodoById(todo.id);
-    }
-    public async deleteTodoById(id: number): Promise<boolean> {
-        try {
-            let res = await this.httpClient.fetch(this.apiConf.basePath + id, {
-                method: 'delete',
-            })
+  }
+  public async deleteTodo(todo: Todo): Promise<boolean> {
+    return await this.deleteTodoById(todo.id);
+  }
+  public async deleteTodoById(id: number): Promise<boolean> {
+    try {
+      let res = await this.httpClient.fetch(this.apiConf.basePath + id, {
+        method: 'delete',
+      })
 
-            return await res.ok;
-        } catch (err) {
-            return false;
-        }
+      return await res.ok;
+    } catch (err) {
+      return false;
     }
+  }
 }
